b84ad551069544428d63f160632f146ffef24930
Ygor Garofalo <igorgarofalo@gmail.com>
2023-11-28 16:31:57 +0100

    fix on ex. 3

--- src/es3/Main3.java
+++ src/es3/Main3.java
@@ -5,24 +5,23 @@ import java.util.Scanner;
 public class Main3 {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        String word2;
 
-        do {
-            System.out.println("Insert a word");
-            word2 = userInput.nextLine();
 
-            if (!word2.equals(":q")) {
-                int i = 0;
-                while (i < word2.length()) {
-                    System.out.print(word2.charAt(i) + ",");
-                    i++;
-                }
+        System.out.println("Insert a word");
+        String word2 = userInput.nextLine();
+
+        if (word2.equals(":q")) {
+            System.out.println("You pressed the escape character!");
+        } else {
+            int i = 0;
+            while (i < word2.length()) {
+                System.out.print(word2.charAt(i) + ",");
+                i++;
             }
 
-        } while (!word2.equals(":q"));
+        }
 
-        System.out.println("You pressed the escape character!");
-        userInput.close();
+
     }
 
 
